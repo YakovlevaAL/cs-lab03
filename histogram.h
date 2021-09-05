1d0be75d22021fa5dbb2b834a13ee91d489c9e1f
Your Name <you@example.com>
2021-09-06 02:48:16 +0300

    Начало lab04

--- histogram.h
+++ histogram.h
@@ -1,8 +1,15 @@
 #pragma once
 
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
 void
 find_minmax(const vector<double>& numbers, double& min, double& max);
+
+vector<size_t>
+make_histogram(const vector<double>& numbers, const size_t count);
+
+void
+show_histogram_text(vector<size_t> bins);
